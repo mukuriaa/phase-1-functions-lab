7543599134c5df762ffae360d5e5ef0919cd7ec6
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-09 04:41:51 -0700

    commit tree 01a4987a6f37c65c21fa62127fb5f8b791ee21ca

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    if (number > 42){
+      return number- 42
+    }else if (number < 42){
+      return 42 - number
+    }
+  }
+
+   
+  function distanceFromHqInFeet(number){
+    return distanceFromHqInBlocks(number) * 264;
+  }
+
+  
+  function distanceTravelledInBlocks(start, end){
+    if (start > end){
+      return start - end
+    } else if (start < end){
+      return end - start
+    }
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    return distanceTravelledInBlocks(start, destination) * 264
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <=400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {;
+      return .02 * (distance-400);
+    } else if (distance> 2000 && distance < 2500){
+      return 25;
+    }  else {
+      return "cannot travel that far"
+    }
+  }
